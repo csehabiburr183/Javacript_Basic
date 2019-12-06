7e2e9eb3b7f625b18e2fa461615337952267427d
csehabiburr183 <csehabiburr183@gmail.com>
2019-12-06 15:48:40 +0600

    Javascript Practice For Absolutely Beginning

---
+++ basic/basic.js
@@ -0,0 +1,17 @@
+/*
+
+Javascript File 
+
+
+It's Multiple Comment Line
+
+*/
+
+// Single Line Comment
+
+
+var habib = "My Name Is Habibur Rahman - It's From Variable";
+document.write(habib + "<hr/>");
+
+
+document.write("Variable is Like That A Container That Contains/Store A Value");
