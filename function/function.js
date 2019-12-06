22bb6217c0f217d6a4b4273df3d215b358417206
csehabiburr183 <csehabiburr183@gmail.com>
2019-12-06 20:10:55 +0600

    Funny Function Added

---
+++ function/function.js
@@ -0,0 +1,28 @@
+// Javascript File
+function Habib(){
+var Maximum_valobasa = "Takar Khela";
+
+if (Maximum_valobasa == "Takar Khela") {
+	return ("Right Kotha")
+}
+else{
+	return("Vul Kotha")
+}
+};
+
+document.querySelector('.TwoAns').innerHTML = Habib();
+
+
+// 2nd Test 
+function Jokes(){
+	var Real_love = "Fucking Always";
+
+	if (Real_love == "Fucking Always") {
+		return ("Baler Real Love")
+	}
+
+	else{
+		return ("Buke Ay Vai. Tokei Khujtecilam")
+	}
+}
+document.querySelector('.Ans').innerHTML = Jokes();
