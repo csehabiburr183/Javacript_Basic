6451e2bfcd99689beb7c94aa8005062895a4654e
csehabiburr183 <csehabiburr183@gmail.com>
2019-12-06 16:06:22 +0600

    Data Type Part Added

---
+++ core.js
@@ -0,0 +1,22 @@
+
+
+// Javascript File 
+
+// String
+var stringType = " It's String Type Data.. All Of Value Insile Double Quote Are String";
+document.querySelector('#Preview1').innerHTML = stringType;
+
+
+// Integer
+var IntegerTypeData = 47125635874 + " -> It's Integer Type Data";
+document.querySelector('#Preview2').innerHTML = IntegerTypeData;
+
+
+// Float
+var FloatTypeData = 54.025 + " -> It's Float Type Data";
+document.querySelector('#Preview3').innerHTML = FloatTypeData;
+
+
+// Boolean
+var CheckData = false;
+document.querySelector('#Preview4').innerHTML = CheckData + " ---- true/false Is Boolean Data";
